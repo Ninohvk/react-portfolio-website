850e7625247ec5cccdd40db8cf5d228b586fef00
Yanina Hernandez <yanina.hernandez.vega@everis.com>
2022-05-01 23:08:30 -0400

    portfolio, testimonials and contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id="contact">contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <container className="contact__container">
+        <div className="contact__options"></div>
+        <form action="">
+          <article className="contact__option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>email@email.com</h5>
+            <a href="mailto:email@email.com" target="_blank" rel="noreferrer">
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>username-messenger</h5>
+            <a
+              href="https://m.me/username-messenger"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp />
+            <h4>Whatsapp</h4>
+            <h5>+56123-456-789</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+56123456789"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </form>
+      </container>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,87 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
+
+const data = [
+  {
+    image: IMG1,
+    title: "Crypto Currency Dashboard & Finantial Visualization",
+    github: "https://www.github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    image: IMG2,
+    title: "Charts templates & infographics in Figma",
+    github: "https://www.github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    image: IMG3,
+    title: "Figma dashboard UI kit for data design web apps",
+    github: "https://www.github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    image: IMG4,
+    title: "Mantaining tasks and tracking progress",
+    github: "https://www.github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    image: IMG5,
+    title: "Charts templates & infographics in Figma",
+    github: "https://www.github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    image: IMG6,
+    title: "Crypto Currency Dashboard & Finantial Visualization",
+    github: "https://www.github.com",
+    demo: "https://dribble.com",
+  },
+];
 
 const Portfolio = () => {
-  return <section id="portforlio">Portfolio</section>;
+  return (
+    <section id="portforlio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {data.map((project, index) => (
+          <article className="portfolio__item" key={index}>
+            <div className="portfolio__item-image">
+              <img src={project.image} alt="imagen" />
+            </div>
+            <h3>{project.title}</h3>
+            <div className="portfolio__item-cta">
+              <a
+                href={project.github}
+                className="btn"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+              <a
+                href={project.demo}
+                className="btn btn-primary"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
