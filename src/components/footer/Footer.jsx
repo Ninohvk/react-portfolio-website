fd983f38f3f581c86b308d8651354d6a2f37b111
Yanina Hernandez <yanina.hernandez.vega@everis.com>
2022-05-02 17:32:57 -0400

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#home" className="footer__logo">
+        MY LOGO
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="www.facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="www.twitter.com">
+          <IoLogoTwitter />
+        </a>
+        <a href="www.instagram.com">
+          <FiInstagram />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; EGATOR Tutorials. All right reserved</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
