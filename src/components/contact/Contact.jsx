92d54965db26f9d5ab1eaab13d131e36096fbf4f
Yanina Hernandez <yanina.hernandez.vega@everis.com>
2022-05-02 15:43:05 -0400

    fix import emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,7 @@ import "./contact.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { RiMessengerLine } from "react-icons/ri";
 import { BsWhatsapp } from "react-icons/bs";
-import emailjs from "@emailjs/browser";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
   const form = useRef();
